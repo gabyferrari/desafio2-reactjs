a7116b4a1791e1441f944ccce455b202d8e29771
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-26 19:43:56 -0300

    Route NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Computers from "./routes/Home/Products/Computers";
 import Eletronics from "./routes/Home/Products/Eletronics";
 import Books from "./routes/Home/Products/Books";
 import AboutUs from "./routes/Home/AboutUs";
+import NotFound from "./routes/Home/NotFound";
 
 export default function App() {
 
@@ -20,6 +21,7 @@ export default function App() {
             <Route path="books" element={<Books />} />
           </Route>
           <Route path="about" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/routes/Home/NotFound/index.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+
+  return (
+    <main className="container mt20">
+      <h2>Não encontrado!</h2>
+    </main>
+  );
+}
