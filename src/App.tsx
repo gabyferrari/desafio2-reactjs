6fc758cc8583c0e3cc8a0f874f627b9934cd8a3b
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-26 16:52:54 -0300

    Basic route structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeIndex from "./routes/Home/HomeIndex";
+import Products from "./routes/Home/Products";
+import Computers from "./routes/Home/Products/Computers";
+import Eletronics from "./routes/Home/Products/Eletronics";
+import Books from "./routes/Home/Products/Books";
+import AboutUs from "./routes/Home/AboutUs";
+
 export default function App() {
 
   return (
-    <h1>Olá</h1>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeIndex />} />
+          <Route path="products" element={<Products />}> 
+            <Route path="computers" element={<Computers />} />
+            <Route path="eletronics" element={<Eletronics />} />
+            <Route path="books" element={<Books />} />
+          </Route>
+          <Route path="about" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
